81e4ae1354d83e9ea84be29f863955b4648f68ac
JungYoungsu <terun707@naver.com>
2019-02-22 15:47:30 +0900

    apply Swagger : setting + controller

--- src/main/java/com/multi/contactsapp/ContactRestController.java
+++ src/main/java/com/multi/contactsapp/ContactRestController.java
@@ -11,20 +11,28 @@ import com.multi.contactsapp.domain.ContactList;
 import com.multi.contactsapp.domain.Result;
 import com.multi.contactsapp.service.ContactService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
 @RestController
 @RequestMapping(value="contacts")
+@Api(value="ContactList Service Controller, 연락처 RESTful 서비스")
 public class ContactRestController {
 	
 	@Autowired
 	private ContactService contactService;
 	
+	@ApiOperation(value="연락처 조회", notes="기존 연락처를 조회합니다. .... ")
 	@RequestMapping(method = RequestMethod.GET)
 	public ContactList getContactList() {
 		return contactService.getContactList();
 	}
-
+	
+	@ApiOperation(value="연락처 추가", notes="연락처를 추가합니다. .... ")
 	@RequestMapping(method = RequestMethod.POST)
-	public Result insertContact(@RequestBody Contact c) {
+	public Result insertContact(
+			@ApiParam(value="추가할 연락처 정보") @RequestBody Contact c) {
 		return contactService.insertContact(c);
 	}
 }
