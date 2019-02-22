6515981941f13153dbc050512283fb093f152dea
JungYoungsu <terun707@naver.com>
2019-02-22 15:51:41 +0900

    apply Swagger : domain class

--- src/main/java/com/multi/contactsapp/domain/Contact.java
+++ src/main/java/com/multi/contactsapp/domain/Contact.java
@@ -5,13 +5,21 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(value="연락처 한 건 객체")
 @XmlRootElement(name="contact")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Contact {
+	@ApiModelProperty(value="일련번호", example="1004")
 	@XmlAttribute
 	private int no;
+	@ApiModelProperty(value="이름", example="홍길동")
 	private String name;
+	@ApiModelProperty(value="전화번호", example="010-2222-3331")
 	private String tel;
+	@ApiModelProperty(value="주소", example="서울시")
 	private String address;
 	public Contact() {
 		super();
